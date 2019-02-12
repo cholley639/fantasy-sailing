3c29d9a8c820cf8948756c242375b1a3e2328d0f
Andrew Savage <andrew.savage.270@gmail.com>
2019-02-12 17:54:39 -0500

    Example how to use requests

--- test_scraper.py
+++ test_scraper.py
@@ -1,3 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def get_html_from_url(url):
+    res = requests.get(url)
+    return res.text
+
+
+print(get_html_from_url('https://scores.collegesailing.org/s19/harris-kempner/sailors/'))
+print('done')
