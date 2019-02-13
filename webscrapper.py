56babb4db9eca1eb4eb346631c145cc03c1e582e
Cam Holley <cameron.holley@tufts.edu>
2019-02-12 22:35:46 -0500

    webscrapper now finds team list

--- webscrapper.py
+++ webscrapper.py
@@ -18,6 +18,9 @@ class="sailor-name skipper"
 
 class="totalcell"
 
+
+
+*** The number of teams in the event can be determined by the length of the lists***
 """
 
 from bs4 import BeautifulSoup
@@ -44,10 +47,29 @@ def get_scores_list(soup):
 	return scores_list
 
 
+def get_teams_list(soup):
+
+	teams_list = []
+
+	for teams in soup.find_all(class_='schoolname'):
+		name = teams.text
+		teams_list.append(name)
+
+	return teams_list
+
+
+
+
 
 if __name__ == '__main__':
     soup = import_page("f18", "coed-showcase", "A")
 
     scores_list = get_scores_list(soup)
-    print scores_list 
+    teams_list = get_teams_list(soup)
+
+    print len(teams_list)
+
+
+
+
 
