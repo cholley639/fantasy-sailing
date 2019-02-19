f3a6b3d795bffac0c335684b2fc6683715b435ad
Cam Holley <cameron.holley@tufts.edu>
2019-02-19 12:04:37 -0500

    first commit for combining full-scores and sailors page

---
+++ sailor_score_per_weekend.py
@@ -0,0 +1,13 @@
+"""
+sailor_score_per_weekend.py
+
+This function/file will be responsible for combining the two data tables
+from the sailors techscore page and the full-scores tech score page.
+
+Final product should look like this:
+
+cont DIDNOTSAIL = 0
+
+ sailor 		|	div	|race 1 | race 2 | .... | TOT
+ --------------------------------------------
+ Jack Bitney 	|	B 	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	6	|	2	|	8	|
