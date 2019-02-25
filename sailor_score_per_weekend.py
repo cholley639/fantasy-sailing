e4505146187325d72c945d50dd2c86b657fc1680
Cam Holley <cameron.holley@tufts.edu>
2019-02-24 21:14:55 -0500

    FullScoresScrapper is the same, per_weekend small update in main

--- sailor_score_per_weekend.py
+++ sailor_score_per_weekend.py
@@ -11,3 +11,22 @@ cont DIDNOTSAIL = 0
  sailor 		|	div	|race 1 | race 2 | .... | TOT
  --------------------------------------------
  Jack Bitney 	|	B 	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	6	|	2	|	8	|
+
+
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import sailors_scrapper
+
+def main(url):
+	sailor_matrix = sailors_scrapper.sailors_matrix(url)
+
+if __name__ == '__main__':
+    regatta_name = 'thompson'
+    techscore_url = 'https://scores.collegesailing.org/s18/{}/sailors/'.format(regatta_name)
+    main(techscore_url)
+
+
---
+++ scoring.py
@@ -0,0 +1,17 @@
+"""
+scoring.py
+
+Fleet Racing Scoring Algorithm
+fantasy-sailing
+
+
+Cameron Holley
+"""
+
+
+#Input Data format
+#Array of scores
+
+
+def sailor_score(arr):
+	
