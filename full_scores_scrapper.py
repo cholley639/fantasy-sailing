2386fc9f9711030e228b0cf4599e411a2c90a9d8
Cam Holley <cameron.holley@tufts.edu>
2019-02-26 14:28:59 -0500

    update function 'scores_matrix' to return data

--- full_scores_scrapper.py
+++ full_scores_scrapper.py
@@ -2,24 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import re
-""" for row in rows:
-        if 'divA' in row['class']:
-            curr_data = {}
 
-        if 'totalrow' not in row['class']:
-            field = row.find_all('td')[2]
-            if field.text.strip():
-                curr_data['teamname'] = field.text.strip()
-
-            for field, name in zip(row.find_all('td')[2:], field_names):
-                if field.text.strip() or name not in curr_data:
-                    curr_data[name] = field.text.strip()
-
-            data.append(list(curr_data.values()))
-"""
-
-def not_totalrows(class_):
-    return class_ and not re.compile("totalrow").search(class_)
 
 
 def get_html_from_url(url):
@@ -34,6 +17,44 @@ def write_to_csv(file_name, headers, data):
         for row in data:
             csv_writer.writerow(row)
 
+
+def scores_matrix(url):
+    raw_html = get_html_from_url(url)
+    soup = BeautifulSoup(raw_html, 'html.parser')
+
+    data = []
+    scores_table = soup.find('table', attrs={'class': 'results coordinate'})
+    scores_body = scores_table.find('tbody')
+
+    temp_header1 =  scores_table.find('thead').find('tr').find_all('th')[2:-2]
+    temp_header2 = scores_table.find('thead').find('tr').find_all('th')[-1:]
+    scores_headers = temp_header1 + temp_header2
+    headers = [ele.text.strip() for ele in scores_headers]
+
+    #add name to headers rank
+
+    rows = scores_body.find_all('tr')
+    teamname = ""
+    for row in rows:
+        if 'totalrow' not in row['class']:
+            if 'divA' in row['class']:
+                teamname = row.find_all('td')[2].text
+
+            arr = []
+            count=0
+            for ele in row.find_all('td')[2:]:
+                if count==0:
+                    arr.append(teamname)
+                elif ele == row.find_all('td')[-2]:
+                    pass
+                else:
+                    arr.append(ele.text)
+                count+=1
+
+            data.append(arr)
+    data.insert(0, headers)
+
+    return data
 
 def main(url, csv_file='thomp.csv'):
     raw_html = get_html_from_url(url)
