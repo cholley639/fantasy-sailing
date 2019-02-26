38c81fcb414a8fe27fb81ccc3843a75dd844d7f3
Cam Holley <cameron.holley@tufts.edu>
2019-02-26 14:28:09 -0500

    imports scores and sailors matrix from other files

---
+++ per_weekend.py
@@ -0,0 +1,41 @@
+"""
+sailor_score_per_weekend.py
+
+This function/file will be responsible for combining the two data tables
+from the sailors techscore page and the full-scores tech score page.
+
+Final product should look like this:
+
+cont DIDNOTSAIL = 0
+
+ sailor 		|	reg	|	div	|race 1 | race 2 | .... | TOT
+ --------------------------------------------
+ Jack Bitney 	|thompson|	B	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	0	|	6	|	2	|	8	|
+
+
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import sailors_scrapper
+import full_scores_scrapper
+
+def main(scores_url, sailors_url):
+	sailor_matrix = sailors_scrapper.sailors_matrix(sailors_url)
+
+	scores_matrix = full_scores_scrapper.scores_matrix(scores_url)
+
+
+
+	print sailor_matrix
+	print "----"
+	print scores_matrix
+
+if __name__ == '__main__':
+    regatta_name = 'thompson'
+    sailors_url = 'https://scores.collegesailing.org/s18/{}/{}/'.format(regatta_name, 'sailors')
+    scores_url = 'https://scores.collegesailing.org/s18/{}/{}/'.format(regatta_name, 'full-scores')
+    main(scores_url, sailors_url)
+
