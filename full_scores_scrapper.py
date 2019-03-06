a8c39f911bdf85f9b5cb8b436c2b63d3b9c5d522
Cam Holley <cameron.holley@tufts.edu>
2019-03-06 14:37:51 -0500

    better documentation for functions and file

--- full_scores_scrapper.py
+++ full_scores_scrapper.py
@@ -1,3 +1,23 @@
+"""
+full_scores_scrapper.py
+
+Cameron Holley
+Winter 2019
+
+This is a program for my fantasy-sailing web app project.
+This file contains the implementation for a webscrapper using BeautifulSoup
+that takes all of the data I need from the full-scores page of a given college
+sailing regatta on the techscore website.
+
+The functions main() and scores_matrix() are very similar. The difference
+is that main() is used to test the functioanlity and output the resulting
+data to a csv. Meanwhile, scores_matrix() is the actual function being imported
+by in the back-end of the app that delivers the data as a matrix to other
+python functions.
+
+"""
+
+
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -5,11 +25,18 @@ import re
 
 
 
+#Param: string url
+#Returns: raw text
+#Does: returns raw html from a given url
 def get_html_from_url(url):
     res = requests.get(url)
     return res.text
 
 
+#Param: output file name, headers row, output data
+#Returns: n/a
+#Does: writes the data to a csv with
+#       the first row as headers
 def write_to_csv(file_name, headers, data):
     with open(file_name, 'w') as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -18,6 +45,11 @@ def write_to_csv(file_name, headers, data):
             csv_writer.writerow(row)
 
 
+#Param: string url
+#Returns: data in matrix form with headers as first row
+#Does: Scrapes the /full-scores/ page of any techscore regatta
+#       for all of the sailing data from that given regatta.
+#       Formats the data as a 2D-array (matrix)
 def scores_matrix(url):
     raw_html = get_html_from_url(url)
     soup = BeautifulSoup(raw_html, 'html.parser')
@@ -55,11 +87,14 @@ def scores_matrix(url):
 
             arr.append(reg_name)
             data.append(arr)
-            
+
     data.insert(0, headers)
 
     return data
 
+
+#   same as scores_matrix, but used to test functionality
+#   and write to csv directly (see notes at top)
 def main(url, csv_file='scores.csv'):
     raw_html = get_html_from_url(url)
     soup = BeautifulSoup(raw_html, 'html.parser')
