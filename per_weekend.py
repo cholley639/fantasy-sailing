5418de054343da14dd8cb2a03f90039e8b450f6c
Cam Holley <cameron.holley@tufts.edu>
2019-03-09 12:25:11 -0500

    IP match scores row with sailor from sailors_matrix
    Problem - the names of universities are different on the two pages ie. "Yale University" vs "Yale"

--- per_weekend.py
+++ per_weekend.py
@@ -8,9 +8,9 @@ Final product should look like this:
 
 cont DIDNOTSAIL = 0
 
- sailor 		|	reg	|	div	|race 1 | race 2 | .... |race 11|race12 |	TOT | fantasy points
+ sailor 		| team	|	reg	 |	div	|race 1 | race 2 | .... |race 11|race12 |	TOT | fantasy points
  --------------------------------------------
- Jack Bitney 	|thompson|	B	|	0	|	0	 | ....	|	6	|	2	|	8	| 
+ Jack Bitney 	| Tufts	|thompson|	B	|	0	|	0	 | ....	|	6	|	2	|	8	| 
 
 
 """
@@ -24,9 +24,16 @@ import full_scores_scrapper
 import re
 
 
-def convert_races(s):
-	s_arr = s.split(',')
+#converts string indicating what races were sailed to easier format
+def convert_races_string(s, scores_row):
+	if not s:	#all races sailed
+		s_arr = scores_row[2:-2]
+	else:
+		s_arr = s.split(',')
+
 	return s_arr
+
+
 
 def main(scores_url, sailors_url):
 	output_matrix = []
@@ -34,19 +41,28 @@ def main(scores_url, sailors_url):
 	scores_matrix = full_scores_scrapper.scores_matrix(scores_url)
 
 	max_num_races = scores_matrix[0][-3]
-	print max_num_races
+	reg_name = scores_matrix[1][-1]
 
 	prev_skipper = ""
 	prev_crew = ""
 	for row in sailors_matrix[1:]:
-		if row[4] != prev_skipper and row[6] != prev_crew:		#new skipper and crew
-			skip_arr = [row[4], scores_matrix[1][-1], row[2]]
-			crew_arr = [row[6], scores_matrix[1][-1], row[2]]
+		scores_row = [] #matches the correct scores for a given sailor from the scores_matrix
+		for iter_score_row in scores_matrix[1:]:
+			if iter_score_row[0] == row[0] and iter_score_row[1] == row[2]:
+				scores_row =iter_score_row
+				break
+		print row[4], row[6], scores_row
 
-			skip_races = convert_races(row[5])
-			crew_races = convert_races(row[7])
-			print skip_races
-			print crew_races
+
+		if row[4] != prev_skipper and row[6] != prev_crew:		#new skipper and crew
+			skip_arr = [row[4], reg_name, row[1]]
+			crew_arr = [row[6], reg_name, row[1]]
+
+			skip_races = convert_races_string(row[5], scores_row)
+
+			crew_races = convert_races_string(row[7], scores_row)
+			#print skip_races
+			#print crew_races
 
 
 		elif row[4] != prev_skipper and row[6] == prev_crew:	#new skipper, same crew
