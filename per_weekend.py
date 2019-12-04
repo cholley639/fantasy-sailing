feb722a62aa9ab04ab5b988ea822172905153f5f
Cam Holley <cameron.holley@tufts.edu>
2019-12-04 13:30:51 -0500

    update filenames

--- per_weekend.py
+++ per_weekend.py
@@ -21,8 +21,8 @@ cont DIDNOTSAIL = 0
 import requests
 from bs4 import BeautifulSoup
 import csv
-import sailors_scrapper
-import full_scores_scrapper
+import sailors_scraper
+import full_scores_scraper
 import re
 
 
@@ -96,7 +96,7 @@ def make_headers(max_num_races):
 	for i in range(1, max_num_races + 1):
 		headers.append(i)
 
-	headers.append('Fantasy Points')
+	#headers.append('Fantasy Points')
 
 	return headers
 
@@ -112,8 +112,8 @@ def make_final_output_rows(name, schoolref, regatta_name, div, scores_row):
 
 def main(scores_url, sailors_url, csv_file='weekend_results.csv'):
 	output_matrix = []
-	sailors_matrix = sailors_scrapper.sailors_matrix(sailors_url)
-	scores_matrix = full_scores_scrapper.scores_matrix(scores_url)
+	sailors_matrix = sailors_scraper.sailors_matrix(sailors_url)
+	scores_matrix = full_scores_scraper.scores_matrix(scores_url)
 
 	max_num_races = int(scores_matrix[0][-4])
 	reg_name = scores_matrix[1][-2]
@@ -156,6 +156,55 @@ def main(scores_url, sailors_url, csv_file='weekend_results.csv'):
 			output_matrix.append(crew_output_arr)
 
 	full_scores_scrapper.write_to_csv(csv_file, headers, output_matrix)
+
+
+def regatta_matrix(scores_url, sailors_url):
+	output_matrix = []
+	sailors_matrix = sailors_scraper.sailors_matrix(sailors_url)
+	scores_matrix = full_scores_scraper.scores_matrix(scores_url)
+
+	max_num_races = int(scores_matrix[0][-4])
+	reg_name = scores_matrix[1][-2]
+
+	headers = make_headers(max_num_races)
+	#output_matrix.append(headers) 
+
+	curr_skipper = ""
+	curr_crew = ""
+	for iter_sailor_row in sailors_matrix[1:]:		# iterates through rows in the sailors csv matrix
+		match_score_row = []  							# creates empty row to be filled with the correct scores row for a given sailor(s)
+		for iter_score_row in scores_matrix[1:]:	# iterates through rows in the scores csv matrix
+			if iter_score_row[-1] == iter_sailor_row[0] and iter_score_row[1] == iter_sailor_row[3]: # matches the correct scores for a given sailor from the scores_matrix
+				match_score_row = iter_score_row													 # first condition checks team, second checks division
+				break
+
+		if iter_sailor_row[5] != curr_skipper and iter_sailor_row[7] != curr_crew:  # new skipper and crew
+			curr_skipper = iter_sailor_row[5]										# update latest seen skipper
+			curr_crew = iter_sailor_row[7]											# update latest seen crew
+
+			skip_races = match_sailor_scores(iter_sailor_row[6], match_score_row[2:-3], max_num_races)	 #race scores for all the races that the given sailor participated in
+			crew_races = match_sailor_scores(iter_sailor_row[8], match_score_row[2:-3], max_num_races)
+
+			skip_output_arr = make_final_output_rows(curr_skipper, iter_score_row[-1], reg_name, iter_sailor_row[3], skip_races)	#name, schoolref, regatta name, div, 
+			crew_output_arr = make_final_output_rows(curr_crew, iter_score_row[-1], reg_name, iter_sailor_row[3], crew_races)
+
+			output_matrix.append(skip_output_arr)
+			output_matrix.append(crew_output_arr)
+
+		elif iter_sailor_row[5] != curr_skipper and iter_sailor_row[7] == curr_crew:  # new skipper, same crew
+			curr_skipper = iter_sailor_row[5]
+			skip_races = match_sailor_scores(iter_sailor_row[6], match_score_row[2:-3], max_num_races)
+			skip_output_arr = make_final_output_rows(curr_skipper, iter_score_row[-1], reg_name, iter_sailor_row[3], skip_races)
+			output_matrix.append(skip_output_arr)
+
+		else:  # same skipper, new crew
+			curr_crew = iter_sailor_row[7]
+			crew_races = match_sailor_scores(iter_sailor_row[8], match_score_row[2:-3], max_num_races)
+			crew_output_arr = make_final_output_rows(curr_crew, iter_score_row[-1], reg_name, iter_sailor_row[3], crew_races)
+			output_matrix.append(crew_output_arr)
+
+
+	return output_matrix
 
 
 
